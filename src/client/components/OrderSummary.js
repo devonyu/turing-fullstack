1b47f872bf57c358db5c3e34465f51fbef0ebef6
Devon Yu <devonyu415@gmail.com>
2019-09-12 23:25:21 -0700

    before sleeping -> adding shipping cost to order summary

--- src/client/components/OrderSummary.js
+++ src/client/components/OrderSummary.js
@@ -1,4 +1,5 @@
 import React from "react";
+import axios from "axios";
 import Button from "@material-ui/core/Button";
 import { makeStyles } from "@material-ui/core/styles";
 // import PropTypes from "prop-types";
@@ -21,9 +22,26 @@ const useStyles = makeStyles(theme => ({
 
 const OrderSummary = props => {
   const { cart, checkout, total } = props;
+  const { shippingID } = checkout.shippingData;
+  const [shippingCosts, setShippingCost] = React.useState([]);
   const classes = useStyles();
+
+  React.useEffect(() => {
+    async function getShippingApi() {
+      try {
+        const response = await axios.get("/api/shipping");
+        const { data } = response;
+        setShippingCost(...data);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+    getShippingApi();
+  }, []);
+
   console.log("ORDER SUMMARY");
   console.log(props);
+  console.log(shippingCosts);
   return (
     <div className={classes.container}>
       <Button
@@ -38,8 +56,22 @@ const OrderSummary = props => {
       <Divider />
       <h1>Order Summary</h1>
       <h3>Items: ${total}</h3>
-      <h3>Shipping and handling: --</h3>
-      <h3>Total before tax: --</h3>
+      <h3>
+        Shipping and handling:
+        {
+          shippingCosts.filter(
+            shippingCost => shippingCost.shipping_id === shippingID
+          ).shipping_cost
+        }
+      </h3>
+      <h3>
+        Total before tax:
+        {
+          shippingCosts.filter(
+            shippingCost => shippingCost.shipping_id === shippingID
+          ).shipping_cost
+        }
+      </h3>
       <h3>Estimated tax to be collected: --</h3>
       <Divider />
       <h3>Order total: --</h3>
